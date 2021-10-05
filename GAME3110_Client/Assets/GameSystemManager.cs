31920da895e4fd444ff598b2dd3adeff19d3ec0e
Eric Galway <ericwilliamgalway@gmail.com>
2021-10-05 02:12:20 -0400

    Added functions to UI elements, can send create/log in info to server

--- GAME3110_Client/Assets/GameSystemManager.cs
+++ GAME3110_Client/Assets/GameSystemManager.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 public class GameSystemManager : MonoBehaviour
 {
     GameObject inputFieldUsername, inputFieldPassword, buttonSubmit, toggleLogIn, toggleCreateAccount;
+    GameObject networkClient;
     // Start is called before the first frame update
     void Start()
     {
         GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
-        
         foreach(GameObject go in allObjects)
         {
             if (go.name == "Name Input Field")
@@ -23,10 +23,18 @@ public class GameSystemManager : MonoBehaviour
                 toggleCreateAccount = go;
             else if (go.name == "Login Toggle")
                 toggleLogIn = go;
+            else if (go.name == "networkClient")
+                networkClient = go;
+            else
+            {
+                Debug.Log("GameObject: " + go.name + " not set to existing Game Object");
+            }
       
         
         }
         buttonSubmit.GetComponent<Button>().onClick.AddListener(SubmitButtonPressed);
+        toggleCreateAccount.GetComponent<Toggle>().onValueChanged.AddListener(ToggleCreateValueChanged);
+        toggleLogIn.GetComponent<Toggle>().onValueChanged.AddListener(ToggleLogInValueChanged);
     }
 
     // Update is called once per frame
@@ -34,8 +42,41 @@ public class GameSystemManager : MonoBehaviour
     {
         
     }
+
     public void SubmitButtonPressed()
     {
-        Debug.Log("Submit button pressed");
+        string n = inputFieldUsername.GetComponent<InputField>().text;
+        string p = inputFieldPassword.GetComponent<InputField>().text;
+
+        if (toggleLogIn.GetComponent<Toggle>().isOn)
+        {
+            networkClient.GetComponent<NetworkedClient>().SendMessageToHost(ClientToSeverSignifiers.Login + "," + n + "," + p);
+        }
+        else
+        {
+            networkClient.GetComponent<NetworkedClient>().SendMessageToHost(ClientToSeverSignifiers.CreateAccount + "," + n + "," + p);
+        }
     }
+
+    public void ToggleCreateValueChanged(bool val)
+    {
+        toggleLogIn.GetComponent<Toggle>().SetIsOnWithoutNotify(!val);
+    }
+
+    public void ToggleLogInValueChanged(bool val)
+    {
+        toggleCreateAccount.GetComponent<Toggle>().SetIsOnWithoutNotify(!val);
+    }
+
+}
+
+public static class ClientToSeverSignifiers
+{
+    public const int Login = 1;
+    public const int CreateAccount = 2;
+}
+
+public static class ServertoClientSignifiers
+{
+    public const int LoginResponse = 1;
 }
